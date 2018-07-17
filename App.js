438e17b214f36377584ab9b00224969af3b0d732
winadilaku6 <winadi@laku6.com>
2018-07-17 14:07:47 +0700

    Adding modal, can delete or close - video #35

--- App.js
+++ App.js
@@ -3,10 +3,12 @@ import { StyleSheet, View } from "react-native";
 
 import PlaceInput from "./src/components/PlaceInput/PlaceInput";
 import PlaceList from "./src/components/PlaceList/PlaceList";
+import PlaceDetail from "./src/components/PlaceDetail/PlaceDetail";
 
 export default class App extends Component {
   state = {
-    places: []
+    places: [],
+    selectedPlace: null
   };
 
   placeAddedHandler = placeName => {
@@ -23,21 +25,43 @@ export default class App extends Component {
     });
   };
 
-  placeDeletedHandler = key => {
+  placeSelectedHandler = key => {
+    this.setState(prevState => {
+      return {
+        selectedPlace: prevState.places.find(place => {
+          return place.key === key;
+        })
+      }
+    })
+
+  }
+
+  placeDeletedHandler = () => {
     this.setState(prevState => {
       return {
         places: prevState.places.filter((place) => {
-          return place.key !== key; //to filter aray, if not equal to the index
-        })
+          return place.key !== prevState.selectedPlace.key; //to filter aray, if not equal to the index
+        }),
+        selectedPlace: null
       }
     });
+  }
+
+  modalClosedHandler = () => {
+    this.setState({
+      selectedPlace: null
+    })
   }
 
   render() {
     return (
       <View style={styles.container}>
+        <PlaceDetail 
+        selectedPlace={this.state.selectedPlace}
+        onItemDeleted={this.placeDeletedHandler}
+        onModalClosed={this.modalClosedHandler}/>
         <PlaceInput onPlaceAdded={this.placeAddedHandler} />
-        <PlaceList places={this.state.places} onItemDeleted={this.placeDeletedHandler} />
+        <PlaceList places={this.state.places} onItemSelected={this.placeSelectedHandler} />
       </View>
     );
   }
--- src/components/PlaceList/PlaceList.js
+++ src/components/PlaceList/PlaceList.js
@@ -10,7 +10,7 @@ const placeList = props => {
         renderItem={(info) => (
             <ListItem placeName={info.item.name} 
             placeImage={info.item.image} 
-            onItemPressed={() => props.onItemDeleted(info.item.key)} />
+            onItemPressed={() => props.onItemSelected(info.item.key)} />
         )}/>
     );
 };
