169fff2a168af01d2c74c22289d10e001ecd713a
winadilaku6 <winadi@laku6.com>
2018-07-17 12:54:22 +0700

    Rendering lists correctly using FlastList - video #32

--- src/components/PlaceList/PlaceList.js
+++ src/components/PlaceList/PlaceList.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { StyleSheet, ScrollView } from 'react-native';
+import { StyleSheet, FlatList } from 'react-native';
 
 import ListItem from '../ListItem/ListItem';
 
 const placeList = props => {
-    const placesOutput = props.places.map((place, i) => (
-        <ListItem key={i} placeName={place} onItemPressed={() => props.onItemDeleted(i)} />
-      ));
     return (
-        <ScrollView style={styles.listContainer}>{placesOutput}</ScrollView>
+        <FlatList style={styles.listContainer} 
+        data={props.places}
+        renderItem={(info) => (
+            <ListItem placeName={info.item.value} onItemPressed={() => props.onItemDeleted(info.item.key)} />
+        )}/>
     );
 };
 
